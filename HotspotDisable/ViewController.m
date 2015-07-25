973cb8c86afece58be5dffd24f79581fc6561de2
james <dickfala@gmail.com>
2015-07-25 09:03:39 +0800

    init

--- HotspotDisable/ViewController.m
+++ HotspotDisable/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SCDynamicStore.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"device name:%@",[[UIDevice currentDevice] name]);
+    
+    SCDynamicStoreRef sc = SCDynamicStoreCreate(NULL, CFSTR("com.apple.wirelessmodemsettings.MISManager"), NULL, NULL);
+    NSDictionary* info = (__bridge_transfer NSDictionary*)SCDynamicStoreCreate(sc, CFSTR("com.apple.MobileInternetSharing"));
+    CFRelease(sc);
+    
 }
 
 - (void)didReceiveMemoryWarning {
